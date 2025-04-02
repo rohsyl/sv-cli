902403f12a4f550d225bd6436ce0f3d781e81111
Sylvain Roh <sylvain@rohs.ch>
2025-04-02 20:34:57 +0200

    feat: installer scripts dont need to install go

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -1,23 +1,3 @@
-# Check if Go is installed
-if (-not (Get-Command "go" -ErrorAction SilentlyContinue)) {
-    Write-Host "Go is not installed. Installing Go..."
-
-    # Determine the latest Go version
-    $goUrl = "https://go.dev/dl/go1.21.1.windows-amd64.msi"
-    $msiPath = "$env:TEMP\go.msi"
-
-    # Download the MSI installer
-    Invoke-WebRequest -Uri $goUrl -OutFile $msiPath
-
-    # Install Go
-    Start-Process msiexec.exe -ArgumentList "/i `"$msiPath`" /quiet /norestart" -Wait
-    Remove-Item $msiPath
-
-    Write-Host "Go has been installed."
-} else {
-    Write-Host "Go is already installed."
-}
-
 # Download sv.exe
 Write-Host "Downloading sv-cli binary..."
 $svUrl = "https://github.com/rohsyl/sv-cli/releases/download/latest/sv.exe"
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,28 +1,5 @@
 #!/bin/bash
 
-# Check if Go is installed
-if ! command -v go &> /dev/null; then
-    echo "Go is not installed. Installing Go..."
-
-    # Determine the latest Go version
-    GO_VERSION="1.21.1"
-    GO_URL="https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz"
-    TEMP_FILE="/tmp/go.tar.gz"
-
-    # Download and install Go
-    curl -Lo "$TEMP_FILE" "$GO_URL"
-    sudo tar -C /usr/local -xzf "$TEMP_FILE"
-    rm -f "$TEMP_FILE"
-
-    # Add Go to PATH
-    echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.bashrc
-    source ~/.bashrc
-
-    echo "Go has been installed."
-else
-    echo "Go is already installed."
-fi
-
 # Download sv binary
 echo "Downloading sv-cli binary..."
 SV_URL="https://github.com/rohsyl/sv-cli/releases/download/latest/sv"
